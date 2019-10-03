27959d59622eb288fde302752175a75d1aae3037
sugerme <hanakazu8989@gmail.com>
2019-10-03 19:06:56 +0900

    fix folder structure

---
+++ final/core/blockchain.py
@@ -0,0 +1,201 @@
+from typing import List
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+import requests
+from urllib.parse import urlparse
+import copy
+from util import sign
+from dns import DNS
+
+class Block(object):
+    def __init__(self, index: int, timestamp: float, transactions: List['Transaction'], proof: int, previous_hash: str):
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.proof = proof
+        self.previous_hash = previous_hash
+
+    def __iter__(self):
+        """
+        transactionsの辞書を返すためにこれを作った
+        """
+        yield ("index", self.index)
+        yield ("timestamp", self.timestamp)
+        yield ("transactions", list(map(lambda t: t.__dict__, self.transactions)))
+        yield ("proof", self.proof)
+        yield ("previous_hash", self.previous_hash)
+
+class Transaction(object):
+    def __init__(self, sender: str, recipient: str, amount: int, timestamp: float, signature: str):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+        self.timestamp = timestamp
+        self.signature = signature
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.nodes = {}
+        self.new_block(
+            previous_hash = 1,
+            transactions = self.current_transactions,
+            proof=100
+        )
+
+
+    def new_block(self, proof: int, transactions: List['Transaction'], previous_hash: str = None):
+        """
+        新しいブロックを作成して追加する
+        :param proof: int
+        :previous_hash: 以前のハッシュ値
+        """
+        block = Block (
+            len(self.chain) + 1,
+            time(),
+            transactions,
+            proof,
+            previous_hash or self.hash(self.chain[-1])
+        )
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender: str, recipient: str, amount: int, timestamp: float, signature: str) -> int:
+        """
+        新しいトランザクションを作成し追加する
+        :param sender: int
+        :param recipient: str
+        :param amount: int
+        :return: int 作成したトランザクションを含むブロックのアドレス
+        """
+        self.current_transactions.append(
+            Transaction(sender, recipient, amount, timestamp, signature)
+        )
+        return self.last_block.index + 1
+
+    def register_node(self, domain, port):
+        """
+        ノードを追加する
+        失敗したら例外を返す
+        :param ip
+        :param port
+        """
+        # 名前解決
+        try:
+            ip = DNS.domain_to_ip(domain)
+        except:
+            ip = domain
+        try:
+            # uuidを取得
+            response = requests.get(f'http://{ip}:{port}/uuid')
+            if response.status_code == 200:
+                self.nodes[f'{ip}:{port}'] = {}
+                uuid = response.json()['uuid']
+                self.nodes[f'{ip}:{port}']['uuid'] = uuid
+            else:
+                raise Exception("GET uuid failed")
+            # publickeyを取得
+            response = requests.get(f'http://{ip}:{port}/publickey')
+            if response.status_code == 200:
+                key = response.json()['key']
+                self.nodes[f'{ip}:{port}']['key'] = key
+            else:
+                raise Exception("GET pubkey failed")
+        except:
+            raise Exception("Cannot register node")
+
+    def proof_of_work(self, last_proof: int) -> int:
+        """
+        PoWを行い，proofを返す
+        :param last_proof: int 最後のブロックのproof
+        :return:int 計算したproofの値
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    def valid_chain(self, chain: List["Block"]) -> bool:
+        """
+        ブロックチェーンが正しければtrue
+        :param chain: List[Block]
+        :return: bool
+        """
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print('\n----------------\n')
+            if block["previous_hash"] != self.hash(last_block):
+                print('bad block: invalid previous hash')
+                print(block["previous_hash"])
+                return False
+            if not self.valid_proof(last_block["proof"], block["proof"]):
+                print('bad block: invalid proof')
+                return False
+            last_block = block
+            current_index += 1
+        return True
+
+    def resolve_conflicts(self):
+        """
+        他のノードとのコンフリクトを解消する
+        """
+        neighbours = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
+        winner_node = ""
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+                    winner_node = node
+        if new_chain:
+            print(f'{chain}')
+            self.chain = [
+                Block(
+                    chain["index"],
+                    chain["timestamp"],
+                    [Transaction(t["sender"], t["recipient"], t["amount"], t["timestamp"], t["signature"]) for t in chain["transactions"]],
+                    chain["proof"],
+                    chain["previous_hash"])
+                for chain in new_chain]
+            response = requests.get(f'http://{winner_node}/transactions')
+            if response.status_code == 200:
+                new_transactions = response.json()['transactions']
+                self.current_transactions = [
+                    Transaction(t["sender"], t["recipient"], t["amount"], t["timestamp"], t["signature"]) for t in new_transactions
+                ]
+            return True
+        return False
+
+    @staticmethod
+    def hash(obj) -> str:
+        obj_string = json.dumps(dict(obj), sort_keys=True).encode()
+        return hashlib.sha256(obj_string).hexdigest()
+
+    @staticmethod
+    def valid_proof(last_proof: int, proof: int) -> bool:
+        """
+        proofの計算を行い，正しければtrueを返す
+        :param last_proof: int
+        :param proof: int
+        :return bool
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:10] < "0000030000"
+
+    @property
+    def last_block(self) -> 'Block':
+        return self.chain[-1]
---
+++ final/core/dns.py
@@ -0,0 +1,149 @@
+import random
+import socket
+
+class DNSFlags():
+    def __init__(self, qr, opcode, aa, tc, rd, ra, z, ad, cd, rcode):
+        self.qr = qr # 1bit
+        self.opcode = opcode # 4bit
+        self.aa = aa # 1bit
+        self.tc = tc # 1bit
+        self.rd = rd # 1bit
+        self.ra = ra # 1bit
+        self.z = z # 1bit
+        self.ad = ad # 1bit
+        self.cd = cd # 1bit
+        self.rcode = rcode # 4bit
+    
+    def to_bytes(self):
+        result = bytearray()
+        result.append(
+            self.qr << 7 | 
+            (self.opcode >> 3 & 0x1) << 6 |
+            (self.opcode >> 2 & 0x1) << 5 |
+            (self.opcode >> 1 & 0x1) << 4 |
+            (self.opcode >> 0 & 0x1) << 3 |
+            self.aa << 2 |
+            self.tc << 1 |
+            self.rd << 0
+        )
+        result.append(
+            self.ra << 7 |
+            self.z << 6 |
+            self.ad << 5 |
+            self.cd << 4 |
+            (self.rcode >> 3 & 0x1) << 3 |
+            (self.rcode >> 2 & 0x1) << 2 |
+            (self.rcode >> 1 & 0x1) << 1 |
+            (self.rcode >> 0 & 0x1) << 0
+        )
+        return bytes(result)
+
+class DNSHeader():
+    def __init__(self, flags, qdcount, ancount, nscount, arcount):
+        self.id = random.randint(0, 10000)
+        self.flags = flags
+        self.qdcount = qdcount
+        self.ancount = ancount
+        self.nscount = nscount
+        self.arcount = arcount
+    
+    def to_bytes(self):
+        result = bytearray()
+        result.append(self.id >> 8 & 0xff)
+        result.append(self.id & 0xff)
+        for b in self.flags.to_bytes():
+            result.append(b)
+        result.append(self.qdcount >> 8 & 0xff)
+        result.append(self.qdcount & 0xff)
+        result.append(self.ancount >> 8 & 0xff)
+        result.append(self.ancount & 0xff)
+        result.append(self.nscount >> 8 & 0xff)
+        result.append(self.nscount & 0xff)
+        result.append(self.arcount >> 8 & 0xff)
+        result.append(self.arcount & 0xff)
+        return bytes(result)
+
+class QuerySection():
+    def __init__(self, domain, dtype, dclass):
+        self.domains = domain.split(".")
+        self.dtype = dtype
+        self.dclass = dclass
+    
+    def to_bytes(self):
+        result = bytearray()
+        for domain in self.domains:
+            result.append(len(domain))
+            for b in bytes(domain.encode()):
+                result.append(b)
+        result.append(0) # 終端文字
+        result.append((self.dtype >> 8) & 0xff)
+        result.append(self.dtype & 0xff)
+        result.append((self.dclass >> 8) & 0xff)
+        result.append(self.dclass & 0xff)
+        return bytes(result)
+
+class DNS():
+    def __init__(self, header, sections, ip="127.0.0.1", port=53):
+        self.header = header
+        self.sections = sections
+        self.address = (ip, port)
+
+    def to_bytes(self):
+        result = bytearray()
+        for b in self.header.to_bytes():
+            result.append(b)
+        for section in self.sections:
+            for b in section.to_bytes():
+                result.append(b)
+        return bytes(result)
+
+    @staticmethod
+    def domain_to_ip(domain):
+        # make packet
+        flags = DNSFlags(
+            qr = 0, # 問い合わせ=0, 応答=1
+            opcode = 0, # 問い合わせ=0, notify=4, update=5
+            aa = 0, # 管理権限の応答=1
+            tc = 0, # 応答が切り詰められた=1
+            rd = 1, # 名前解決するかどうか
+            ra = 0, # 名前解決可能=1, 非サポート=0
+            z = 0, # 予約（常に0)
+            ad = 0, # 問い合わせの際に応答のadが理解できる=1, 応答の際DNSSECの検証が成功=1
+            cd = 0, # 問い合わせの際，DNSSECの検証を無効=1
+            rcode = 0 # 応答コード(NOERROR, SERVFAIL, NXDOMAIN, REFUSED)
+        )
+        header = DNSHeader(
+            flags = flags,
+            qdcount = 1, # 問い合わせセクション数
+            ancount = 0, nscount = 0, arcount = 0
+        )
+        sections = []
+        sections.append(QuerySection(
+            domain = domain,
+            dtype = 1, # A record = 0x0001
+            dclass = 1, # IN = 0x0001
+        ))
+        dns = DNS(header, sections)
+
+        # communicate
+        BUFFER = 1024
+        query = dns.to_bytes()
+        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # internet, udp
+        try:
+            client.settimeout(2)
+            client.sendto(query, dns.address)
+            data, address = client.recvfrom(BUFFER)
+            client.close()
+        except socket.timeout:
+            client.close()
+            raise Exception('DNS timeout.')
+        # get answer
+        rcode = data[3] & 0xf
+        if rcode == 0:
+            answer = data[len(query)+12:len(query)+16]
+            return "%d.%d.%d.%d" % (answer[0], answer[1], answer[2], answer[3])
+        else:
+            raise Exception('error code %d: cannot resolve domain. ' % rcode) 
+    @staticmethod
+    def ip_to_domain():
+        pass
